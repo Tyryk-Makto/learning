a92f5b15c4fd7d6930566c74645743c0fa66ab6d
Tyryk-Makto <gerasimov_evgeny@inbox.ru>
2025-01-23 14:10:42 +0300

    for

---
+++ Лотерея 2.py
@@ -0,0 +1,3 @@
+for tickets in 345, 19, 87, 1020, 421:
+    if tickets < 1000 and (tickets % 5 == 0):
+        print('Выгрышный билет: ', tickets)
---
+++ Таблица степеней.py
@@ -0,0 +1,4 @@
+for numbers in 3, 7, 5, 6, 4, 9:
+    print(numbers ** 2, numbers ** 3, numbers ** 4)
+
+
